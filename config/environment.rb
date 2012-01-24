5e5a2619164afa5ad865774e4064041b2e9ff908
Tilman Porschuetz <tilman@sinnfrei.co.uk>
2012-01-24 18:49:28 +0100

    moved syck innitialization to from boot.rb to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,9 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 require 'radius'
 
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
+
 Radiant::Initializer.run do |config|
   # Skip frameworks you're not going to use (only works if using vendor/rails).
   # To use Rails without a database, you must remove the Active Record framework
